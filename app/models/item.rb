bb3afa146f9de343c1e68516ad59112d47f11584
Tomek Meus <70977982+old-twister@users.noreply.github.com>
2022-11-02 20:40:58 +0100

    Item type added. Item type show works in index view. Item type select does not work in 'new item' view

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
-  validates :name, presence: true, length: {minimum: 3}
+  validates :name, presence: true, length: { minimum: 3 }
+
+  enum item_type: %i[book cd electric_device flower other]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_27_181617) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_02_170749) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_181617) do
     t.boolean "borrowed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "item_type", default: 4, null: false
   end
 
 end

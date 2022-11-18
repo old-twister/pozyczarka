35bb781d357615fe7a10c326eac9a7a6b2844659
Tomek Meus <70977982+old-twister@users.noreply.github.com>
2022-11-18 17:17:16 +0100

    Fix rails best practices warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,10 +13,7 @@ RSpec.configure do |config|
   config.color = true
   config.tty = true
   config.formatter = :documentation
-
   config.filter_run_when_matching :focus
-  #  config.example_status_persistence_file_path = "spec/examples.txt"
-  #  config.disable_monkey_patching!
 
   if config.files_to_run.one?
     config.default_formatter = "doc"

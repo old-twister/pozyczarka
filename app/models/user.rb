7e5bb8e34dcebf904b2da7325eb522283fc440e7
Tomek Meus <70977982+old-twister@users.noreply.github.com>
2023-01-02 14:46:08 +0100

    Znowu działa

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticable, :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :items, dependent: :destroy
-  has_many :friendships, inverse_of: :user, dependent: :destroy
-  has_many :friends, through: friendships
 end

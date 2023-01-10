8ff9c018d7ad08fd85fafcf3f6f16bc731a8b4c4
Tomek Meus <70977982+old-twister@users.noreply.github.com>
2023-01-10 09:38:07 +0100

    Navigation from notification added

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -2,20 +2,28 @@ class Invitation < ApplicationRecord
   include AASM
 
   aasm :column => 'state' do
-  state :pending, initial: true
-  state :accepted, :rejected
+    state :pending, initial: true
+    state :accepted, :rejected
 
-  event :accept do
-    transitions from: :pending, to: :accepted
-  end
+    event :accept do
+      transitions from: :pending, to: :accepted
+    end
 
-  event :reject do
-    transitions from: :pending, to: :rejected
-  end
+    event :reject do
+      transitions from: :pending, to: :rejected
+    end
   end
 
   belongs_to :invited_user, class_name: 'User', foreign_key: :invited_id, optional: true
   belongs_to :inviting_user, class_name: 'User', foreign_key: :invited_by_id, optional: true
 
   scope :not_processed, -> { where(state: 'pending')}
+
+  after_create do
+    invited_user.notifications.create(data: {
+      message: "#{inviting_user.email} has invited you to friends.",
+      type: 'invitation',
+      path: '/friends'
+    })
+  end
 end
